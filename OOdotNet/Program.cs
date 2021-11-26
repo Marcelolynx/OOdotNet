2bc7dadd2f13322594368570cc6f8ccaabaf2798
marcelolynx <mco02jgp>
2021-11-26 18:53:24 -0400

    First commit OOdotNet

---
+++ OOdotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOdotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
